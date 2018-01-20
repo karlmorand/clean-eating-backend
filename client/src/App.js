7c3189b0815f7b455c955c87bbbeaa0a3b1224fb
Karl Morand <karljmorand@gmail.com>
2018-01-20 12:41:48 -0500

    Basic auth setup of new client

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		console.log('App props: ', this.props);
+		this.goTo = this.goTo.bind(this);
+		this.login = this.login.bind(this);
+		this.logout = this.logout.bind(this);
+	}
+	goTo(route) {
+		this.props.history.replace(`/${route}`);
+	}
+	login() {
+		this.props.auth.login();
+	}
+	logout() {
+		this.props.auth.logout();
+	}
+
+	render() {
+		const { isAuthenticated } = this.props.auth;
+		return (
+			<div className="App">
+				<button onClick={() => this.goTo('home')}>Home</button>
+				{!isAuthenticated() && <button onClick={this.login}>Login</button>}
+				{isAuthenticated() && <button onClick={this.logout}>Logout</button>}
+			</div>
+		);
+	}
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import 'semantic-ui-css/semantic.min.css';
+import { makeMainRoutes } from './routes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const routes = makeMainRoutes();
+
+ReactDOM.render(routes, document.getElementById('root'));
