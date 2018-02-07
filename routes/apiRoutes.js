f2164901fecb98acd6c06a1eb1da2d9b038da93f
Karl Morand <karljmorand@gmail.com>
2018-02-06 20:20:01 -0500

    Remove scope checks for now since they're not required right now

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,15 +6,15 @@ const entriesController = require('../controllers/entriesController.js');
 const usersController = require('../controllers/usersController.js');
 const gymsController = require('../controllers/gymsController.js');
 
-router.get('/dailyentry/existing/:entryId', checkJwt, jwtAuthz(['athlete']), entriesController.getExistingEntry);
+router.get('/dailyentry/existing/:entryId', checkJwt, entriesController.getExistingEntry);
 //Get today's daily entry if one exists, if not create one and return it
 // TODO: Update this to get a daily entry based on a date passed
-router.get('/dailyentry/:id/:date', checkJwt, jwtAuthz(['athlete']), entriesController.getDailyEntry);
+router.get('/dailyentry/:id/:date', checkJwt, entriesController.getDailyEntry);
 
-router.post('/user/:id/setup', checkJwt, jwtAuthz(['athlete']), usersController.userSetup);
+router.post('/user/:id/setup', checkJwt, usersController.userSetup);
 router.get('/user/:id/history', checkJwt, usersController.getHistory);
 //Update a daily entry based on the id of the entry
-router.post('/dailyentry/:entryId', checkJwt, jwtAuthz(['athlete']), entriesController.updateDailyEntryScore);
+router.post('/dailyentry/:entryId', checkJwt, entriesController.updateDailyEntryScore);
 
 router.get('/user/:id', checkJwt, usersController.getUserProfile);
 
