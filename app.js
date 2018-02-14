682d6b835454580b412d5c868de7d2a8883a6af3
Karl Morand <karljmorand@gmail.com>
2018-02-14 16:54:47 -0500

    Require Team schema

--- app.js
+++ app.js
@@ -1,54 +1,55 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-require('dotenv').config({ path: 'variables.env' });
-const mongoose = require('mongoose');
-const User = require('./models/User.js');
-const DailyEntry = require('./models/DailyEntry.js');
-const Gym = require('./models/Gym.js');
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+require("dotenv").config({ path: "variables.env" });
+const mongoose = require("mongoose");
+const User = require("./models/User.js");
+const DailyEntry = require("./models/DailyEntry.js");
+const Gym = require("./models/Gym.js");
+const Team = require("./models/Team.js");
 //Auth middleware from Auth0 guide
-const cors = require('cors');
-const apiRoutes = require('./routes/apiRoutes');
+const cors = require("cors");
+const apiRoutes = require("./routes/apiRoutes");
 
 var app = express();
 app.use(cors());
 
 if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
-	throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file';
+  throw "Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file";
 }
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.use('/api', apiRoutes);
+app.use(express.static(path.join(__dirname, "client/build")));
+app.use("/api", apiRoutes);
 
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname + '/client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-	// render the error page
-	// TODO: fix this since i don't have a view engine server side
-	res.status(err.status || 500);
-	res.render('error');
+  // render the error page
+  // TODO: fix this since i don't have a view engine server side
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
