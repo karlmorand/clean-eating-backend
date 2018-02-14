7a2d1c6113ccf30687fce5ebfc141604d86d6dec
Karl Morand <karljmorand@gmail.com>
2018-02-14 16:53:27 -0500

    Add team to user model

--- models/User.js
+++ models/User.js
@@ -1,38 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		trim: true,
-		required: 'Please supply a name'
-	},
-	email: {
-		type: String,
-		trim: true
-	},
-	picture: {
-		type: String,
-		trim: true
-	},
-	authId: {
-		type: String,
-		required: true
-	},
-	roles: [String],
-	currentQuestions: [],
-	challengeLevel: {
-		type: Number,
-		min: 0,
-		default: 1
-	},
-	gym: {
-		type: mongoose.Schema.ObjectId,
-		ref: 'Gym'
-	},
-	onboardingComplete: {
-		type: Boolean,
-		default: false
-	}
+  name: {
+    type: String,
+    trim: true,
+    required: "Please supply a name"
+  },
+  email: {
+    type: String,
+    trim: true
+  },
+  picture: {
+    type: String,
+    trim: true
+  },
+  authId: {
+    type: String,
+    required: true
+  },
+  roles: [String],
+  currentQuestions: [],
+  challengeLevel: {
+    type: Number,
+    min: 0,
+    default: 1
+  },
+  gym: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Gym"
+  },
+  team: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Team"
+  },
+  onboardingComplete: {
+    type: Boolean,
+    default: false
+  }
 });
 
-module.exports = mongoose.model('User', userSchema, 'users');
+module.exports = mongoose.model("User", userSchema, "users");
