de6f01c50502bbac5dc84b77c8c3e9d201f8c477
Karl Morand <karljmorand@gmail.com>
2018-02-19 23:05:11 -0500

    Remove members, add food guide

--- models/Gym.js
+++ models/Gym.js
@@ -1,26 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const gymSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		trim: true,
-		required: 'Please supply a name'
-	},
-	members: [
-		{
-			type: mongoose.Schema.ObjectId,
-			ref: 'User'
-		}
-	],
-	currentQuestions: [
-		{
-			questionTitle: String,
-			description: String,
-			maxDailyPoints: Number,
-			startingValue: Number,
-			questionType: String //YESNO, SLIDER, TEXTENTRY
-		}
-	]
+  name: {
+    type: String,
+    trim: true,
+    required: "Please supply a name"
+  },
+  currentQuestions: [
+    {
+      questionTitle: String,
+      description: String,
+      maxDailyPoints: Number,
+      startingValue: Number,
+      questionType: String //YESNO, SLIDER, TEXTENTRY
+    }
+  ],
+  foodGude: {
+    type: String,
+    trim: true
+  }
 });
 
-module.exports = mongoose.model('Gym', gymSchema, 'gyms');
+module.exports = mongoose.model("Gym", gymSchema, "gyms");
